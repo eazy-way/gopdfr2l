ce461dc717358884bb5b753794c9a1d77b72f50b
yukimimiya <taguchi949@gmail.com>
2022-09-23 10:58:13 +0900

    update

--- gopdfr2l.go
+++ gopdfr2l.go
@@ -13,27 +13,27 @@ func main() {
 	var outfile = flag.String("o","output.pdf","output file name")
 
 	flag.Parse()
-	ctx, err := ReadContextFile(infile)
+	ctx, err := api.ReadContextFile(*infile)
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
-	rootDict, err := ctx.xRefTable.Catalog()
+	xRefTable := ctx.XRefTable
+	rootDict, err := xRefTable.Catalog()
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
 	rootDict.Insert("ViewerPreferences",
-		Dict(
+		pdfcpu.Dict(
 			map[string]Object{
 				"FitWindow":    Boolean(true),
 				"CenterWindow": Boolean(true),
-			}
-		)
+			},
+		),
 	)
-	if err := WriteContextFile(ctx, outfile); err != nil {
+	if err := api.WriteContextFile(ctx, *outfile); err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
-)
 }
